a4b25f4f53dbedac306155b391166440f04deeb1
VIP <VIP@3rd.VIP>
2022-03-15 19:04:24 +0300

    Printing program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+1. #include <stdio.h>
+2. /** 
+4. ** _putchar - writes the character c to stdout
+5. @c: The character to print
+6.
+7. Return: On success 1.
+8.  On error, -1 is returned, and errno is set appropriately.
+9. */
+10.int _putchar(char c)
+11.{
+12.return(write(1, &c, 1));
+13.}
+14.
+15.
+16.
+17.
+18.

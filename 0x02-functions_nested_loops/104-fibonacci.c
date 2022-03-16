cb1b4301b8d1d311bdd09e986da65ade04f8f683
VIP <VIP@3rd.VIP>
2022-03-17 01:55:58 +0300

    functions and nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
+ * Return: 0
+ */
+
+int main(void)
+{
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+
+	printf("1");
+
+	for (counter = 2; counter < 93; counter++) /* will still print */
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
+	}
+

9748599d9f90ed03dccce2a6df4f40bff05ef98c
VIP <VIP@3rd.VIP>
2022-03-30 23:14:51 +0300

    natural squareroot of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @root: test this root
+ */
+
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_root(n, 0));
+}
